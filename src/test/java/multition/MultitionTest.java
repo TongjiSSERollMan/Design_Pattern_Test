42808ce8e6279bf3d16514435a8959b451dfd250
Zact <zactwu@gmail.com>
2020-12-02 21:36:22 +0800

    update zact's test code

--- src/test/java/multition/MultitionTest.java
+++ src/test/java/multition/MultitionTest.java
@@ -3,18 +3,14 @@ package multition;
 import junit.framework.TestCase;
 import org.junit.Test;
 
-/**
- * @author 1851594王思桐
- */
 public class MultitionTest extends TestCase  {
     @Test
-    public  void test() {
-        System.out.println("Multition Test:");
-
-        for(int i = 0;i<5;i++){
+    public void test() {
+        System.out.println("Multition Test Start");
+        for(int i = 0;i<4;i++){
             System.out.println("\t"+ChairMultition.getInstance(i).getInfo());
         }
         System.out.println("\t"+ChairMultition.getInstance(190));
-        System.out.println("Exit Multition Test.");
+        System.out.println("Multition Test Finish");
     }
 }
