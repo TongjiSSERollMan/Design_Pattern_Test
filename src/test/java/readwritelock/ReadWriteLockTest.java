b2552513bcbe6f3bb188906c3ef8aa0003507e67
aodethri <740950342@qq.com>
2020-12-03 20:03:01 +0800

    Finish tests

--- src/test/java/readwritelock/ReadWriteLockTest.java
+++ src/test/java/readwritelock/ReadWriteLockTest.java
@@ -7,30 +7,16 @@ public class ReadWriteLockTest extends TestCase{
 
     @Test
     public void test() {
-        System.out.println("ReadWriteLockTest:");
-
-        System.out.println("Creating Film object...");
-        Film film = new Film("《Frozen》");
-        System.out.println();
-
-        System.out.println("Creating audienceThread object...");
+        Film film = new Film("film1");
         AudienceThread audience1= new AudienceThread(film,"audience1");
         AudienceThread audience2= new AudienceThread(film,"audience2");
-        AudienceThread audience3= new AudienceThread(film,"audience3");
-        System.out.println();
-
-        System.out.println("Creating repairmanThread object...");
         RepairManThread repairMan= new RepairManThread(film);
-        System.out.println();
-
         audience1.start();
         audience2.start();
-        audience3.start();
         repairMan.start();
         try {
             audience1.join();
             audience2.join();
-            audience3.join();
             repairMan.join();
         }
         catch ( InterruptedException e){
